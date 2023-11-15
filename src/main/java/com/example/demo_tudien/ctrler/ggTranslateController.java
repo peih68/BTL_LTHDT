a3e695618469d1bfc4301ce5720edfefd179f0d1
hoaha110405 <hoaha110404@gmail.com>
2023-11-15 22:55:49 +0700

    tudien4.5
    update googlesearch

--- src/main/java/com/example/demo_tudien/ctrler/ggTranslateController.java
+++ src/main/java/com/example/demo_tudien/ctrler/ggTranslateController.java
@@ -1,13 +1,19 @@
 package com.example.demo_tudien.ctrler;
 
 import com.example.demo_tudien.ggApi.Translator;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ggTranslateController {
+public class ggTranslateController implements Initializable {
     @FXML
     TextArea chuaDichTextArea;
 
@@ -23,6 +29,12 @@ public class ggTranslateController {
     @FXML
     Button phatAmDaDichButton;
 
+    @FXML
+    ComboBox<String> langluageWordTarget;
+
+    @FXML
+    ComboBox<String> langluageWordMeaning;
+
     public void onActionPhatAmChuaDichButton() {
         Translator.textToSpeech(chuaDichTextArea.getText(),Translator.Vietnamese);
     }
@@ -32,7 +44,36 @@ public class ggTranslateController {
     }
 
     public void onActionTranslateButton() throws IOException {
-        String str = Translator.translate(Translator.Vietnamese,Translator.English,chuaDichTextArea.getText());
+        String target = langluageWordTarget.getValue();
+        String meaning = langluageWordMeaning.getValue();
+        if(target== null||meaning == null) {
+            langluageWordTarget.setValue("Vietnamese");
+            langluageWordMeaning.setValue("English");
+            target = langluageWordTarget.getValue();
+            meaning = langluageWordMeaning.getValue();
+        }
+        String str = Translator.translate(Translator.languages.get(target),Translator.languages.get(meaning),chuaDichTextArea.getText());
         daDichTextArea.setText(str);
+    }
+
+    @FXML
+    public void switchType()  {
+        String target = langluageWordTarget.getValue();
+        String meaning = langluageWordMeaning.getValue();
+        if(target== null||meaning == null) {
+            langluageWordTarget.setValue("Vietnamese");
+            langluageWordMeaning.setValue("English");
+            target = langluageWordTarget.getValue();
+            meaning = langluageWordMeaning.getValue();
+        }
+        langluageWordTarget.setValue(meaning);
+        langluageWordMeaning.setValue(target);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ObservableList<String> items = FXCollections.observableArrayList(Translator.languageList);
+        langluageWordTarget.setItems(items);
+        langluageWordMeaning.setItems(items);
     }
 }
