6f5eefbfee9e4f04a4f7d51701c2eac66d677f3d
hoaha110405 <hoaha110404@gmail.com>
2023-11-23 21:43:33 +0700

    .

--- src/main/java/com/example/demo_tudien/Main.java
+++ src/main/java/com/example/demo_tudien/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         try {
-            stage.initStyle(StageStyle.UNDECORATED);
+            //stage.initStyle(StageStyle.UNDECORATED);
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/Views/Dictionary_.fxml"));
             Scene scene = new Scene(fxmlLoader.load(), 850, 600);
             stage.setTitle("Từ điển");
